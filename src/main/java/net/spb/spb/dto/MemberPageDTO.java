a40f9881b68bf5ab5d237aaa4b15d457e57d6507
godjihye <njih112@gmail.com>
2025-05-05 03:36:22 +0900

    [Refactor] 페이징 메서드 제네릭화 및 PostPageDTO, MemberPageDTO -> PageDTO 상속 구조로 개선

--- src/main/java/net/spb/spb/dto/MemberPageDTO.java
+++ src/main/java/net/spb/spb/dto/MemberPageDTO.java
@@ -3,6 +3,7 @@ package net.spb.spb.dto;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Positive;
 import lombok.*;
+import lombok.experimental.SuperBuilder;
 
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
@@ -13,65 +14,26 @@ import java.nio.charset.StandardCharsets;
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
-public class MemberPageDTO {
+@SuperBuilder
+public class MemberPageDTO extends PageDTO{
 
     private String search_member_state;
     private String search_member_grade;
-    private String search_word;
-
-    @Positive
-    @Min(0)
-    @Builder.Default
-    private int page_no=1;
-
-    @Positive
-    @Min(0)
-    @Builder.Default
-    private int page_size=10;
-
-    @Positive
-    @Min(0)
-    @Builder.Default
-    private int page_block_size=10;
 
     @Builder.Default
     private String sort_by = "memberIdx";
     @Builder.Default
     private String sort_direction = "desc";
 
-    private int total_count;
-    private String linkUrl;
-
-    public int getPage_skip_count() {
-        return (page_no-1)*page_size;
-    }
-    public int getTotal_page() {
-        return (int) Math.ceil((double) total_count / page_size);
-    }
-    public int getStart_page() {
-        return ((page_no-1)/page_block_size)*page_block_size+1;
-    }
-    public int getEnd_page() {
-        int end = getStart_page() + page_block_size -1;
-        return Math.min(end, getTotal_page());
-    }
-    public boolean isHas_prev() {
-        return getStart_page() > 1;
-    }
-    public boolean isHas_next() {
-        return getEnd_page() < getTotal_page();
-    }
-
     public String toQueryString() {
         return String.format(
                 "search_member_state=%s&search_member_grade=%s&search_type=%s&search_word=%s&page_no=%d&page_size=%d&page_block_size=%d&sort_by=%s",
                 encode(search_member_state),
                 encode(search_member_grade),
-                encode(search_word),
-                page_no,
-                page_size,
-                page_block_size,
+                encode(super.getSearch_word()),
+                super.getPage_no(),
+                super.getPage_size(),
+                super.getPage_block_size(),
                 encode(sort_by)
         );
     }
