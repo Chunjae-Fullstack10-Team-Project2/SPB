61f6307135b5e03999dc5a9e6c23882ad2496a14
godjihye <njih112@gmail.com>
2025-05-12 11:04:22 +0900

    [Fix] 게시글 신고 메세지 수정

--- src/main/java/net/spb/spb/controller/BoardController.java
+++ src/main/java/net/spb/spb/controller/BoardController.java
@@ -239,7 +239,7 @@ public class BoardController {
             result.put("message", "신고가 접수되었습니다.");
         } else {
             result.put("success", false);
-            result.put("message", "신고 접수에 실패했습니다.");
+            result.put("message", "이미 신고한 게시글입니다.");
         }
 
         return result;
