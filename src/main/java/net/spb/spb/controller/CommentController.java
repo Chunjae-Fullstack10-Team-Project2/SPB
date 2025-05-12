fe7c3993d6e2fcbf9290ae6b1df89577c5219b17
godjihye <njih112@gmail.com>
2025-05-13 01:19:24 +0900

    [Refactor] 자유게시판 게시글 댓글 - 댓글 등록 시 페이지 리로드 대신 dom에 추가 - FE 댓글 삭제, 수정 로직 개선 - 유효성 체크

--- src/main/java/net/spb/spb/controller/CommentController.java
+++ src/main/java/net/spb/spb/controller/CommentController.java
@@ -1,86 +1,123 @@
 package net.spb.spb.controller;
 
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
 import net.spb.spb.dto.post.PostCommentDTO;
 import net.spb.spb.service.board.CommentServiceIf;
 import net.spb.spb.util.BoardCategory;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/board/{category}/comment")
 @RequiredArgsConstructor
-@Log4j2
 public class CommentController {
 
     private final CommentServiceIf service;
 
     @PostMapping("/write")
     @ResponseBody
-    public Map<String, Object> write(@PathVariable("category") BoardCategory category, @ModelAttribute PostCommentDTO postCommentDTO, HttpSession session) {
+    public Map<String, Object> write(@PathVariable("category") BoardCategory category,
+                                                @Valid @ModelAttribute PostCommentDTO postCommentDTO,
+                                                BindingResult bindingResult,
+                                                HttpSession session,
+                                                @RequestParam("memberProfileImg") String memberProfileImg) {
 
-        Map<String, Object> result = new HashMap<>();
+        Map<String, Object> response = new HashMap<>();
+
+        if (bindingResult.hasErrors()) {
+            String firstErrorMessage = bindingResult.getFieldErrors().get(0).getDefaultMessage();
+            response.put("success", false);
+            response.put("message", firstErrorMessage);
+            return response;
+        }
+
         String sessionMemberId = (String) session.getAttribute("memberId");
         postCommentDTO.setPostCommentMemberId(sessionMemberId);
-        int rtnResult = service.insertComment(postCommentDTO);
-        if (rtnResult > 0) {
-            result.put("success", true);
-            result.put("message", "댓글을 등록하였습니다.");
+        int result = service.insertComment(postCommentDTO);
+        postCommentDTO.setPostCommentMemberProfileImg(memberProfileImg);
+        postCommentDTO.setPostCommentIdx(result);
+        if (result > 0) {
+            response.put("success", true);
+            response.put("message", "댓글이 등록되었습니다.");
+            response.put("comment", postCommentDTO);
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            response.put("createdAt", LocalDateTime.now().format(formatter));
         } else {
-            result.put("success", false);
-            result.put("message", "댓글 등록에 실패하였습니다.");
+            response.put("success", false);
+            response.put("message", "댓글 등록에 실패했습니다.");
         }
-        return result;
+        return response;
     }
 
     @PostMapping("/modify")
     @ResponseBody
-    public Map<String, Object> modify(@PathVariable("category") BoardCategory category, @ModelAttribute PostCommentDTO postCommentDTO, HttpSession session) {
+    public Map<String, Object> modify(@PathVariable("category") BoardCategory category,
+                                      @Valid @ModelAttribute PostCommentDTO postCommentDTO,
+                                      BindingResult bindingResult,
+                                      HttpSession session) {
 
-        Map<String, Object> result = new HashMap<>();
+        Map<String, Object> response = new HashMap<>();
+        if (bindingResult.hasErrors()) {
+            String firstErrorMessage = bindingResult.getFieldErrors().get(0).getDefaultMessage();
+            response.put("success", false);
+            response.put("message", firstErrorMessage);
+            return response;
+        }
         String sessionMemberId = (String) session.getAttribute("memberId");
         if (!sessionMemberId.equals(postCommentDTO.getPostCommentMemberId())) {
-            result.put("success", false);
-            result.put("message", "권한이 없습니다.");
-            return result;
+            response.put("success", false);
+            response.put("message", "권한이 없습니다.");
+            return response;
         }
-        int rtnResult = service.modifyComment(postCommentDTO);
-
-        if (rtnResult > 0) {
-            result.put("success", true);
-            result.put("message", "댓글을 수정하였습니다.");
+        int result = service.modifyComment(postCommentDTO);
+        if (result > 0) {
+            response.put("success", true);
+            response.put("message", "댓글을 수정하였습니다.");
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            response.put("updatedAt", LocalDateTime.now().format(formatter));
         } else {
-            result.put("success", false);
-            result.put("message", "댓글 수정에 실패하였습니다.");
+            response.put("success", false);
+            response.put("message", "댓글 수정에 실패하였습니다.");
         }
-        return result;
+        return response;
     }
 
     @PostMapping("/delete")
     @ResponseBody
-    public Map<String, Object> delete(@PathVariable("category") BoardCategory category, @ModelAttribute PostCommentDTO postCommentDTO, HttpSession session) {
+    public Map<String, Object> delete(@PathVariable("category") BoardCategory category,
+                                      @ModelAttribute PostCommentDTO postCommentDTO,
+                                      HttpSession session) {
 
-        Map<String, Object> result = new HashMap<>();
+        Map<String, Object> response = new HashMap<>();
+        if (postCommentDTO.getPostCommentIdx()<1) {
+            response.put("success", false);
+            response.put("message", "잘못된 요청입니다.");
+        }
         String sessionMemberId = (String) session.getAttribute("memberId");
         if (!sessionMemberId.equals(postCommentDTO.getPostCommentMemberId())) {
-            result.put("success", false);
-            result.put("message", "권한이 없습니다.");
-            return result;
+            response.put("success", false);
+            response.put("message", "권한이 없습니다.");
+            return response;
         }
-
-        int rtnResult = service.deleteComment(postCommentDTO.getPostCommentIdx());
-        if (rtnResult > 0) {
-            result.put("success", true);
-            result.put("message", "댓글이 삭제되었습니다.");
+        int result = service.deleteComment(postCommentDTO.getPostCommentIdx());
+        if (result > 0) {
+            response.put("success", true);
+            response.put("message", "댓글이 삭제되었습니다.");
         } else {
-            result.put("success", false);
-            result.put("message", "댓글 삭제에 실패했습니다.");
+            response.put("success", false);
+            response.put("message", "댓글 삭제에 실패했습니다.");
         }
-        return result;
+        return response;
     }
 }
