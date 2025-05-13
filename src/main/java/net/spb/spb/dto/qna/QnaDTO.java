96c035bb212d02e52741d4836ff8bf3272f193de
sunminnnnn <sslspplz@naver.com>
2025-05-14 00:01:29 +0900

    [design] global css에 body 배경색상 지정

--- src/main/java/net/spb/spb/dto/qna/QnaDTO.java
+++ src/main/java/net/spb/spb/dto/qna/QnaDTO.java
@@ -17,13 +17,13 @@ public class QnaDTO {
     @Size(max = 100, message = "제목은 100자 이하여야 합니다.")
     private String qnaTitle;
 
-    @NotBlank(message = "내용은 필수 입력값입니다.")
     @Size(max = 20, message = "작성자 ID는 20자 이하여야 합니다.")
     private String qnaQMemberId;
 
     @Size(max = 20, message = "답변자 ID는 20자 이하여야 합니다.")
     private String qnaAMemberId;
 
+    @NotBlank(message = "내용은 필수 입력값입니다.")
     @Size(max = 19000, message = "질문 내용은 19,000자 이하여야 합니다.")
     private String qnaQContent;
 
